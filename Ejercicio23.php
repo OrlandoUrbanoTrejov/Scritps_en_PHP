6f6572704042f60dc3c29120511dfa0b2f2b01c0
OrlandoUrbanoTrejov <urbanoorlando79@gmail.com>
2023-08-23 19:07:05 -0600

    Determinar si eres mayor de edad

---
+++ Ejercicio23.php
@@ -0,0 +1,16 @@
+<?php
+/* Orlando Urbano Trejo (Lando)
+ * Fecha: 22-08-2023
+ * Correo: urbanoorlando79@gmail.com
+ */
+/* Ejercicio:
+ * Genera un programa que determine si eres mayor de edad */
+echo "Edad: ";
+$edad = intval(readline());
+if($edad >= 18){
+	echo "Eres mayor de edad"."\n";	
+}
+else{
+	echo "Eres menor de edad"."\n";	
+}
+?>
