426a3a2f3361b5978c0b6fef9bb00df6cfc83898
OrlandoUrbanoTrejov <urbanoorlando79@gmail.com>
2023-08-23 18:44:09 -0600

    Multiplicacion de dos numeros

---
+++ Ejercicio11.php
@@ -0,0 +1,14 @@
+<?php
+/* Orlando Urbano Trejo (Lando)
+ * Fecha: 22-08-2023
+ * Correo: urbanoorlando79@gmail.com
+ *
+ * Ejercicio:
+ * Multiplicar dos numeros */
+echo "Ingresa un numero: ";
+$numero1 = readline();
+echo "Ingresa un otro numero: ";
+$numero2 = readline();
+$solucion = $numero1 * $numero2;
+echo "Resultado: ".$solucion."\n";
+?>
