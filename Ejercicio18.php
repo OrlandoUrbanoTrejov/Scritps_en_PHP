446cf56be8c97c1444d034cae0eeedded445bcc7
OrlandoUrbanoTrejov <urbanoorlando79@gmail.com>
2023-08-23 19:02:58 -0600

    Calcular el suldo total de un empleado

---
+++ Ejercicio18.php
@@ -0,0 +1,15 @@
+<?php
+/* Orlando Urbano Trejo (Lando)
+ * Fecha: 22-08-2023
+ * Correo: urbanoorlando79@gmail.com
+ *
+ * Ejercicio:
+ * Escriba un algoritmo que, dado el número de horas trabajadas por un empleado y el sueldo por hora, calcule el sueldo total de ese empleado */
+echo "Horas trabajadas: ";
+$horas = readline();
+echo "Sueldo por hora: ";
+$sueldo = readline();
+$total = $horas * $sueldo;
+echo "Sueldo total: $".$total."\n";
+
+?>
