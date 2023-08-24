a175b94c3074c8ed20eaaf7146bdfda32be4c7ca
OrlandoUrbanoTrejov <urbanoorlando79@gmail.com>
2023-08-23 19:04:39 -0600

    Concatenar 3 numeros o letras y mostrar el resultado

---
+++ Ejercicio20.php
@@ -0,0 +1,16 @@
+<?php
+/* Orlando Urbano Trejo (Lando)
+ * Fecha: 22-08-2023
+ * Correo: urbanoorlando79@gmail.com
+ *
+ * Ejercicio:
+ * Concatenar 3 numeros o letras y mostrar el resultado */
+echo "Numero 1: ";
+$numero1 = readline();
+echo "Numero 2: ";
+$numero2 = readline();
+echo "Numero 3: ";
+$numero3 = readline();
+$suma = $numero1 + $numero2 + $numero3;
+echo "Suma: ".$suma."\n";
+?>
